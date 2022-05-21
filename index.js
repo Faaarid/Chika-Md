4711df3c17a1356363794edf83652c20493a34b9
Faaarid <90858028+Faaarid@users.noreply.github.com>
2022-05-21 14:00:48 +0700

    Update index.js
    Yeue

--- index.js
+++ index.js
@@ -152,10 +152,7 @@ module.exports = chika = async (chika, m, chatUpdate, store) => {
         }
 
         // Push Message To Console && Auto Read
-        if (m.message) {
-            chika.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ PESAN ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> Dari'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> Di'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
-        }
+     
 	
 	// reset limit every 12 hours
         let cron = require('node-cron')
